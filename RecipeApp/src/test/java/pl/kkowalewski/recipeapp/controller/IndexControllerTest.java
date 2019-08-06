e4fdcac38de6a1bb82d5a500d2cfe87a4564b932
Kamil Kowalewski <szamil128@onet.pl>
2019-08-06 21:03:10 +0200

    Add tests to UnitOfMeasureRepository

--- RecipeApp/src/test/java/pl/kkowalewski/recipeapp/controller/IndexControllerTest.java
+++ RecipeApp/src/test/java/pl/kkowalewski/recipeapp/controller/IndexControllerTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import pl.kkowalewski.recipeapp.model.Recipe;
 import pl.kkowalewski.recipeapp.service.RecipeService;
@@ -15,6 +18,9 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -32,6 +38,13 @@ public class IndexControllerTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void mockMvcTest() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(view().name(IndexController.DESCRIPTION));
     }
 
     @Test
---
+++ RecipeApp/src/test/java/pl/kkowalewski/recipeapp/repository/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,32 @@
+package pl.kkowalewski.recipeapp.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.kkowalewski.recipeapp.model.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryTest {
+
+    /*------------------------ FIELDS REGION ------------------------*/
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    /*------------------------ METHODS REGION ------------------------*/
+    @Test
+    public void findByDescription() {
+        String description = "Teaspoon";
+        Optional<UnitOfMeasure> unitOfMeasureOptional =
+                unitOfMeasureRepository.findByDescription(description);
+
+        assertEquals(description, unitOfMeasureOptional.get().getDescription());
+    }
+}
+    
