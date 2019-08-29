81318092b63118c77743b1d48cc79e322df5fc13
Kamil Kowalewski <szamil128@onet.pl>
2019-08-29 11:22:21 +0200

    Add recipeform

--- RecipeApp/src/main/java/pl/kkowalewski/recipeapp/controller/RecipeController.java
+++ RecipeApp/src/main/java/pl/kkowalewski/recipeapp/controller/RecipeController.java
@@ -2,8 +2,11 @@ package pl.kkowalewski.recipeapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kkowalewski.recipeapp.command.RecipeCommand;
 import pl.kkowalewski.recipeapp.service.RecipeService;
 
 @Controller
@@ -11,6 +14,8 @@ public class RecipeController {
 
     /*------------------------ FIELDS REGION ------------------------*/
     public static final String RECIPE_SHOW = "/recipe/show";
+    public static final String RECIPE_NEW = "recipe/new";
+    public static final String RECIPE_RECIPE_FORM = "recipe/recipeform";
     public static final String ATTRIBUTE = "recipe";
 
     private final RecipeService recipeService;
@@ -25,5 +30,18 @@ public class RecipeController {
         model.addAttribute(ATTRIBUTE, recipeService.findById(Long.valueOf(id)));
 
         return RECIPE_SHOW.substring(1);
+    }
+
+    @RequestMapping(RECIPE_NEW)
+    public String newRecipe(Model model) {
+        model.addAttribute(ATTRIBUTE, new RecipeCommand());
+
+        return RECIPE_RECIPE_FORM;
+    }
+
+    @PostMapping
+    @RequestMapping(ATTRIBUTE)
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        return "redirect:" + RECIPE_SHOW + "/" + recipeService.saveRecipeCommand(recipeCommand).getId();
     }
 }
